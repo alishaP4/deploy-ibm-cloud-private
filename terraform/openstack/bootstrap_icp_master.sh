d55658ad927bb6b75702f9880bf1bc1a27b3b617
alishaP4 <alisha_prabhu@persistent.com>
2018-11-13 17:41:45 +0530

    Update bootstrap_icp_master.sh

--- terraform/openstack/bootstrap_icp_master.sh
+++ terraform/openstack/bootstrap_icp_master.sh
@@ -159,9 +159,9 @@ if [ "${if_HA}" == "true" ]; then
     /bin/mkdir -p /var/lib/registry
     /bin/mkdir -p /var/lib/icp/audit
     /bin/mkdir -p /var/log/audit
-    /bin/mount -o tcp,mountproto=tcp,nfsvers=3 $reg_path $registry_mount_src
-    /bin/mount -o tcp,mountproto=tcp,nfsvers=3 $auth_audit_path $audit_mount_src
-    /bin/mount -o tcp,mountproto=tcp,nfsvers=3 $kub_audit_path $kub_audit_mount_src
+    /usr/bin/mount -o tcp,mountproto=tcp,nfsvers=3 "${reg_path}" "${registry_mount_src}"
+    /usr/bin/mount -o tcp,mountproto=tcp,nfsvers=3 "${auth_audit_path}" "${audit_mount_src}"
+    /usr/bin/mount -o tcp,mountproto=tcp,nfsvers=3 "${kub_audit_path}" "${kub_audit_mount_src}"
 fi
 
 if [ "${if_HA}" == "false" ]; then
