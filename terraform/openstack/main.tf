2f0ccbdafce7610c1ff5d2126847a301bc00ec91
alishaP4 <alisha_prabhu@persistent.com>
2018-11-14 23:13:18 +0530

    Update main.tf

--- terraform/openstack/main.tf
+++ terraform/openstack/main.tf
@@ -84,21 +84,6 @@ resource "openstack_compute_instance_v2" "icp-master-vm" {
     
     #user_data = "${data.template_file.bootstrap_init.rendered}"
     user_data = "${count.index > 0 ? "${data.template_file.bootstrap_init_subsequent_masters.rendered}" : "${data.template_file.bootstrap_init.rendered}"}" 
-	
-    #NFS server should be mounted on all the master nodes
-    #inline = [
-      #"sudo mkdir -p /var/lib/registry",
-      #"sudo mkdir -p /var/lib/icp/audit",
-      #"sudo mkdir -p /var/log/audit",
-      #"mount $registry_mount_options $reg_path $registry_mount_src",
-      #"mount $audit_mount_options $auth_audit_path $audit_mount_src",
-      #"mount $kub_audit_mount_options $kub_audit_path $kub_audit_mount_src"
-      #commented below lines coz of syntax error
-      #"echo '${var.registry_mount_src} /var/lib/registry  ${var.registry_mount_type}  ${var.registry_mount_options}   0 0' | sudo tee -a /etc/fstab",
-      #"echo '${var.audit_mount_src} /var/lib/icp/audit   ${var.audit_mount_type}  ${var.audit_mount_options}  0 0' | sudo tee -a /etc/fstab",
-      #"echo '${var.kub_audit_mount_src} /var/log/audit   ${var.kub_audit_mount_type}  ${var.kub_audit_mount_options}  0 0' | sudo tee -a /etc/fstab",
-      #"sudo mount -a"
-    #]
 
 }
 
@@ -150,6 +135,55 @@ data "template_file" "bootstrap_worker" {
     }
 }
 
+resource "openstack_compute_instance_v2" "icp-proxy-vm" {
+    count     = "${var.icp_num_proxy}"
+    name      = "${format("${var.instance_prefix}-proxy-${random_id.rand.hex}-%02d", count.index+1)}"
+    image_id  = "${var.openstack_image_id}"
+    flavor_id = "${var.openstack_flavor_id_proxy_node}"
+    key_pair  = "${openstack_compute_keypair_v2.icp-key-pair.name}"
+    security_groups = ["${var.openstack_security_groups}"]
+    availability_zone = "${var.openstack_availability_zone}"
+
+    network {
+        name = "${var.openstack_network_name}"
+    }
+
+    user_data = "${data.template_file.bootstrap_proxy.rendered}"
+    //local exec will cm for proxy if adding/deleting it is required.
+}
+
+data "template_file" "bootstrap_proxy" {
+    template = "${file("bootstrap_icp_proxy.sh")}"
+
+    vars {
+        docker_download_location = "${var.docker_download_location}"
+    }
+}
+
+resource "openstack_compute_instance_v2" "icp-management-vm" {
+    count     = "${var.icp_num_management}"
+    name      = "${format("${var.instance_prefix}-management-${random_id.rand.hex}-%02d", count.index+1)}"
+    image_id  = "${var.openstack_image_id}"
+    flavor_id = "${var.openstack_flavor_id_management_node}"
+    key_pair  = "${openstack_compute_keypair_v2.icp-key-pair.name}"
+    security_groups = ["${var.openstack_security_groups}"]
+    availability_zone = "${var.openstack_availability_zone}"
+
+    network {
+        name = "${var.openstack_network_name}"
+    }
+
+    user_data = "${data.template_file.bootstrap_management.rendered}"
+    //local exec will cm for management if adding/deleting it is required.
+}
+
+data "template_file" "bootstrap_management" {
+    template = "${file("bootstrap_icp_management.sh")}"
+
+    vars {
+        docker_download_location = "${var.docker_download_location}"
+    }
+}
 #...........................................null resourse for master....................................
 
 resource "null_resource" "icp-master-scaler" {
@@ -207,6 +241,38 @@ resource "null_resource" "icp-worker-scaler" {
     provisioner "file" {
         content     = "${join("|", openstack_compute_instance_v2.icp-worker-vm.*.network.0.fixed_ip_v4)}"
         destination = "/tmp/icp_worker_nodes.txt"
+    }
+
+    provisioner "file" {
+        content     = "${file("${var.openstack_ssh_key_file}")}"
+        destination = "/tmp/id_rsa.terraform"
+    }
+}
+
+#...............................................null resource for proxy.................................
+
+resource "null_resource" "icp-proxy-scaler" {
+    triggers {
+        workers = "${join("|", openstack_compute_instance_v2.icp-proxy-vm.*.network.0.fixed_ip_v4)}"
+    }
+
+    connection {
+        type            = "ssh"
+        user            = "${var.icp_install_user}"
+        #host            = "${openstack_compute_instance_v2.icp-proxy-vm.*.network.0.fixed_ip_v4}"
+        host            = "${openstack_compute_instance_v2.icp-proxy-vm.0.network.0.fixed_ip_v4}"
+        private_key     = "${file(var.openstack_ssh_key_file)}"
+        timeout         = "15m"
+    }
+
+    #provisioner "file" {  				#check if u can add/delete proxy nodes
+        #source      = "${path.module}/icp_proxy_scaler.sh"
+        #destination = "/tmp/icp_proxy_scaler.sh"
+    #}
+
+    provisioner "file" {
+        content     = "${join("|", openstack_compute_instance_v2.icp-proxy-vm.*.network.0.fixed_ip_v4)}"
+        destination = "/tmp/icp_proxy_nodes.txt"
     }
 
     provisioner "file" {
